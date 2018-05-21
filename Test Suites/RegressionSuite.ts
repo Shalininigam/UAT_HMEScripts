10a4fc09e7087b664472fa1491b92a722d123aa5
Shalini Nigam (BLR TT) <shalinin@nousinfo.com>
2018-05-21 12:14:14 +0530

    Updated Test Scripts

--- Test Suites/RegressionSuite.ts
+++ Test Suites/RegressionSuite.ts
@@ -4,13 +4,19 @@
    <name>RegressionSuite</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-05-21T12:09:29</lastRun>
+   <lastRun>2018-05-21T12:11:28</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>9d0b627b-a75b-4186-b869-581fc87ea0f0</testSuiteGuid>
+   <testCaseLink>
+      <guid>e8209e50-d4e9-468e-965c-841250a672cb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TimeZoneTC/HME-Account_Setting-Timezone-06</testCaseId>
+   </testCaseLink>
    <testCaseLink>
       <guid>3039c603-b670-4c2b-9bb8-883c66786c21</guid>
       <isReuseDriver>false</isReuseDriver>
